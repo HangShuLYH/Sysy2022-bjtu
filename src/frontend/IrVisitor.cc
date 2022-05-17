99fa75fa7bae9c408efe265d09c38722f335dfb6
lokol5579 <1239580962@qq.com>
2022-05-17 20:44:09 +0800

    修复了数组为空或部分为空情况下存储错误的bug

--- src/frontend/IrVisitor.cc
+++ src/frontend/IrVisitor.cc
@@ -185,6 +185,18 @@ void IrVisitor::visit(ConstInitVal *constInitVal) {
                 }
             }
         }
+
+        if(!flag && var->arrayDims[dims - 1] - constInitVal->constInitValList.size())
+        {
+            int cnt(var->arrayDims[dims - 1] - constInitVal->constInitValList.size());
+            for(int i(dims); i < tempDims; i++)
+            {
+                cnt *= var->arrayDims[i];
+            }
+            
+            index += cnt;
+        }
+
         dims--;
 
         if(dims == 0)
@@ -414,6 +426,18 @@ void IrVisitor::visit(InitVal *initVal) {
                 }
             }
         }
+
+        if(!flag && var->arrayDims[dims - 1] - initVal->initValList.size())
+        {
+            int cnt(var->arrayDims[dims - 1] - initVal->initValList.size());
+            for(int i(dims); i < tempDims; i++)
+            {
+                cnt *= var->arrayDims[i];
+            }
+            
+            index += cnt;
+        }
+
         dims--;
 
         if(dims == 0)
