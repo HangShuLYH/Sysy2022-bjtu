179dc0ce757695889d0bc9ec86ccfb2489474b80
HangShuLYH <19291212@bjtu.edu.cn>
2022-07-26 14:56:33 +0800

    fix bugs of save r0 - r4

--- src/backend/codegen.cc
+++ src/backend/codegen.cc
@@ -94,8 +94,8 @@ void Codegen::generateProgramCode() {
                         allUsedRegsFR.insert(fr);
                     }
                     if (typeid(*instr) == typeid(Bl)) {
-                        Push* pushInstr = dynamic_cast<Push*>(*(it-1));
-                        Pop* popInstr = dynamic_cast<Pop*>(*(it+1));
+                        Push* pushInstr = dynamic_cast<Push*>(*(it+1));
+                        Pop* popInstr = dynamic_cast<Pop*>(*(it-1));
                         if (pushInstr && popInstr) {
                             pushInstr->addRegs(callerSave);
                             popInstr->addRegs(callerSave);
